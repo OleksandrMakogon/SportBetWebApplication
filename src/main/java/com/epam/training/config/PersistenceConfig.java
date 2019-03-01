a7c24a63467fe9b35cbe2cf90c52f1ab89faba3f
Oleksandr Makogon <wicked2007@ukr.net>
2019-03-01 12:39:30 +0200

    update

--- src/main/java/com/epam/training/config/PersistenceConfig.java
+++ src/main/java/com/epam/training/config/PersistenceConfig.java
@@ -3,20 +3,16 @@ package com.epam.training.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManagerFactory;
-
 import javax.sql.DataSource;
 
 @Configuration
